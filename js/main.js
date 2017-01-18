c780b154a35a2e791bbb24a30b0e2c06345855e7
cosmo <cosmo>
2017-01-18 13:39:56 -0500

    subiendo prototipos

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+function validation () {
+
+	var user= document.getElementById('user').value;
+	var password= document.getElementById('password').value;
+
+
+	if(user == "") {
+		alert('Por favor ingrese el usuario');
+	
+	}
+
+	if(password == "") {
+		alert('Por favor ingrese la contaseña');
+	
+	}
+}
+
+var users = ["user1", "user2", "user3"];
+var passwords = ["pass1", "pass2", "pass3"]
+
+
+function match () {
+
+}
